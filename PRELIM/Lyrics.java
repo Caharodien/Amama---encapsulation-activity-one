bb6744084490fcac9c18dbeb832ba0c9925a9816
Caharodien <120312568+Caharodien@users.noreply.github.com>
2023-10-25 09:25:05 +0800

    Add files via upload

---
+++ PRELIM/Lyrics.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Lyrics{
+public static void main(String []args){
+System.out.println("PERFECT by Edsheeran");
+System.out.println(" ");
+System.out.println("I found a love, for me");
+System.out.println("Well, I found a girl, beautiful and sweet");
+System.out.println("Oh, I never knew you were the someone waiting for me");
+System.out.println(" ");
+System.out.println("'Cause we were just kids when we fell in love");
+System.out.println("Not knowing what it was");
+System.out.println("I will not give you up this time");
+System.out.println("But darling, just kiss me slow");
+System.out.println("Your heart is all I own");
+System.out.println("And in your eyes, you're holding mine");
+System.out.println(" ");
+System.out.println("Baby, I'm dancing in the dark");
+System.out.println("With you between my arms");
+System.out.println("Barefoot on the grass");
+System.out.println("Listening to our favourite song");
+System.out.println("When you said you looked a mess");
+System.out.println("I whispered underneath my breath");
+System.out.println("But you heard it");
+System.out.println("Darling, you look perfect tonight");
+
+}
+}
